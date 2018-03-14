b89567564ed3cd3fa8b58efc4eb20b35e5d011d9
bryan <bmonue1@my.westga.edu>
2018-03-13 22:17:50 -0400

    Created Temperature class + getters + setters

--- app/src/main/java/bryan_caleb/weatherapp/Temperature.java
+++ app/src/main/java/bryan_caleb/weatherapp/Temperature.java
@@ -5,4 +5,40 @@ package bryan_caleb.weatherapp;
  */
 
 public class Temperature {
+    String date;
+    String lowTemp;
+    String highTemp;
+    String url;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLowTemp() {
+        return lowTemp;
+    }
+
+    public void setLowTemp(String lowTemp) {
+        this.lowTemp = lowTemp;
+    }
+
+    public String getHighTemp() {
+        return highTemp;
+    }
+
+    public void setHighTemp(String highTemp) {
+        this.highTemp = highTemp;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
